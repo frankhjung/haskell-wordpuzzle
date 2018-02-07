46d2991dd543ea979043dbc3af2260231d0a5655
Frank Jung <frankhjung@linux.com>
2018-02-07 21:27:59 +1100

    basic working version

--- app/Main.hs
+++ app/Main.hs
@@ -3,17 +3,11 @@
 
 module Main(main) where
 
--- import qualified Data.Text    as Text
--- import qualified Data.Text.IO as Text
 import           Lib
 
 main :: IO ()
 main = do
-    -- dictionary <- fmap Text.lines (Text.readFile "words")
-    dictionary <- fmap lines (readFile "words")
-    -- let p = filterWords 4 'c' "adevcrsoi"
-    -- words <- filter p dictionary
-    -- putStrLn $ "Found" ++ show (length words) ++ " valid words"
-    putStrLn $ "From dictionary of " ++ show (length dictionary) ++ " words ..."
-    -- print a sample of dictionary
-    mapM_ putStrLn ( take 5 dictionary )
+  dictionary <- readFile "words"
+  let p = filterWords 4 'c' "adevcrsoi"
+  mapM_ putStrLn $
+    filter p (lines dictionary)
